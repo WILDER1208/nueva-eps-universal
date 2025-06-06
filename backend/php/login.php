4d696f6fd0e3d2e6ce4f86fa7df3a11592aa0112
wilder betancourt <wilderman08betancourt@gmail.com>
2025-06-06 18:02:12 -0500

    nueva eps universal

---
+++ backend/php/login.php
@@ -0,0 +1,68 @@
+<?php 
+    require '../backend/bd/Conexion.php';
+
+    if(isset($_POST['login'])) {
+    $errMsg = '';
+
+    // Get data from FORM
+    $username = $_POST['username'];
+    
+    $password = MD5($_POST['password']);
+
+    if($username == '')
+      $errMsg = 'Digite su usuario';
+    if($password == '')
+      $errMsg = 'Digite su contraseña';
+
+    if($errMsg == '') {
+      try {
+$stmt = $connect->prepare('SELECT id, username, name, email, password, rol FROM users WHERE username = :username');
+
+
+        $stmt->execute(array(
+          ':username' => $username
+          
+          
+          ));
+        $data = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if($data == false){
+          $errMsg = "El usuario: $username no se encuentra , puede solicitarlo con el administrador.";
+        }
+        else {
+          if($password == $data['password']) {
+            
+
+            $_SESSION['id'] = $data['id'];
+            $_SESSION['username'] = $data['username'];
+            $_SESSION['name'] = $data['name'];
+            $_SESSION['email'] = $data['email'];
+            $_SESSION['password'] = $data['password'];
+            $_SESSION['rol'] = $data['rol'];
+           
+            if($_SESSION['rol'] == 1){
+    header('Location: admin/escritorio.php');
+    exit;
+}
+elseif($_SESSION['rol'] == 2){
+    header('Location: admin/escritorio.php');
+    exit;
+}
+elseif($_SESSION['rol'] == 3){
+    header('Location: admin/escritorio.php');
+    exit;
+}
+else {
+    $errMsg = 'Rol no reconocido.';
+}
+
+            
+          
+}}
+      }
+      catch(PDOException $e) {
+        $errMsg = $e->getMessage();
+      }
+    }
+  }
+ ?>
